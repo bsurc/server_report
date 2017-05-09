3beb6160619021555e15894dfb6d392fd7730b69
Tyler Bevan <tylerbevan@boisestate.edu>
2017-05-09 09:39:01 -0600

    Updated user report to show gigabytes transferred and sort by that metric. Also added a header line.

--- func.py
+++ func.py
@@ -18,16 +18,17 @@ def user_frequency(endpointid, startdate, enddate):
     con = lite.connect(':memory:')
     with con:
         cur = con.cursor()
-        cur.execute("CREATE TABLE People(Name TEXT)")
+        cur.execute("CREATE TABLE People(Name TEXT, Bytes INTEGER)")
         if startdate == '' and enddate == '':
             for task in tc.endpoint_manager_task_list(num_results=None, filter_endpoint=endpointid):
-                cur.execute("INSERT INTO People VALUES(\'{}\')".format(task["owner_string"]))
+                cur.execute("INSERT INTO People VALUES(\'{}\', {})".format(task["owner_string"],task["bytes_transferred"]))
         else:
             for task in tc.endpoint_manager_task_list(num_results=None, filter_endpoint=endpointid, filter_completion_time="{},{}".format(str(startdate), str(enddate))):
-                cur.execute("INSERT INTO People VALUES(\'{}\')".format(task["owner_string"]))
-        cur.execute("SELECT Name, count(*) FROM People GROUP BY Name ORDER BY count(*) DESC")
+                cur.execute("INSERT INTO People VALUES(\'{}\', {})".format(task["owner_string"],task["bytes_transferred"]))
+        cur.execute("SELECT Name, count(Name), sum(Bytes) FROM People GROUP BY Name ORDER BY sum(Bytes) DESC")
+        print('Jobs\tGigabytes\tUser')
         for row in cur:
-            print('{}\t{}'.format(row[1], row[0]))
+            print('{}\t{}\t{}'.format(row[1], str(float(row[2])/1000000000), row[0]))
 
 def job_count(endpointid, startdate, enddate):
     tc = auth.authenticate()
