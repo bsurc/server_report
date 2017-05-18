f1d466b1a551ff352201604e84d7fbc2d4a40aad
Tyler Bevan <tylerbevan@boisestate.edu>
2017-05-18 09:45:07 -0600

    First attempt at listing errored jobs
    Ideally I would use the nice_status text to filter, but that feature is not yet implemented in globus. I have the capability in the code, but for now I will use a less accurate method based on status.

--- server_report/func.py
+++ server_report/func.py
@@ -14,7 +14,6 @@ import server_report.auth as auth
 import logging
 
 log = logging.getLogger(__name__)
-insertstring = "INSERT INTO People VALUES(\'{}\', {})"
 
 
 def list_endpoints():
@@ -28,6 +27,7 @@ def list_endpoints():
 
 def user_frequency(epid, startdate, enddate):
     """Return a list of users on an endpoint sorted by data moved."""
+    insertstring = "INSERT INTO People VALUES(\'{}\', {})"
     log.info("Aquiring transfer access token from refresh token...")
     tc = auth.authorize_transfer()
     log.info("Creating database in memory...")
@@ -95,3 +95,47 @@ def running_count(endpointid):
             filter_status="ACTIVE"):
         count += 1
     print("{}".format(count))
+
+
+def errored_jobs(epid, startdate, enddate):
+    """Return a list of errors on an endpoint."""
+    insertstring = ("INSERT INTO Tasks VALUES(\'{}\', \'{}\', "
+                    "\'{}\', \'{}\')")
+    log.info("Aquiring transfer access token from refresh token...")
+    tc = auth.authorize_transfer()
+    log.info("Creating database in memory...")
+    con = lite.connect(':memory:')
+    with con:
+        emtl = tc.endpoint_manager_task_list
+        cur = con.cursor()
+        log.info("Creating Table...")
+        cur.execute("CREATE TABLE Tasks(Name TEXT, TaskID TEXT, Status TEXT, "
+                    "NiceStatus TEXT)")
+        if startdate == '' and enddate == '':
+            log.info("Inserting tasks without date filtering...")
+            for task in emtl(
+                    num_results=None, filter_endpoint=epid):
+                log.debug(insertstring.format(
+                    task["owner_string"], task["task_id"],
+                    task["status"], task["nice_status"]))
+                cur.execute(insertstring.format(
+                    task["owner_string"], task["task_id"],
+                    task["status"], task["nice_status"]))
+        else:
+            log.info("Inserting tasks with date filtering...")
+            for task in emtl(num_results=None,
+                             filter_endpoint=epid,
+                             filter_completion_time="{},{}".format(
+                                 str(startdate), str(enddate))):
+                log.debug(insertstring.format(
+                    task["owner_string"], task["task_id"],
+                    task["status"], task["nice_status"]))
+                cur.execute(insertstring.format(
+                    task["owner_string"], task["task_id"],
+                    task["status"], task["nice_status"]))
+        log.info("Executing SQL to gather statistics...")
+        cur.execute("SELECT Name, TaskID, Status, NiceStatus "
+                    "FROM Tasks WHERE Status NOT IN "
+                    "('SUCCEEDED', 'ACTIVE')")
+        for row in cur:
+            print('{0}\t{1}\t{2}\t{3}'.format(row[0],row[1],row[2],row[3]))
