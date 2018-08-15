fe04b0c81df8d2053fa0104be5c2f546eebc388d
Tyler Bevan <tylerbevan@boisestate.edu>
2018-08-15 11:34:48 -0600

    Fixed pickle code to comply with python 3.6.

--- server_report/auth.py
+++ server_report/auth.py
@@ -18,29 +18,27 @@ import logging
 log = logging.getLogger(__name__)
 
 CLIENT_ID = 'cca6968a-cc55-4ee9-a651-b66f059037bf'
+auth_client = globus_sdk.auth.AuthClient(client_id=CLIENT_ID)
 directory = '{}/.server_report/'.format(os.environ['HOME'])
 tokenfile = '{}/.server_report/token.dat'.format(os.environ['HOME'])
-
 
 def request_token():
     """Run OAuth flow and store response in pickle for future use."""
     # Create client profile and start OAuth flow
-    client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
-    # We want refresh tokens for
-    client.oauth2_start_flow(refresh_tokens=True)
+    client = globus_sdk.auth.GlobusNativeAppFlowManager(auth_client, refresh_tokens=True)
     # Request authorization URL and prompt user
-    authorize_url = client.oauth2_get_authorize_url()
+    authorize_url = client.get_authorize_url()
     print('Please go to this URL and login: {0}'.format(authorize_url))
     # Get auth code response from user
     auth_code = prompt("Please enter the code you get after login here: ").strip()
-    token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+    token_response = client.exchange_code_for_tokens(auth_code)
     # Save token to file using pickle, overwriting old creds
     if not os.path.exists(directory):
         os.makedirs(directory)
     if os.path.isfile(tokenfile):
         os.remove(tokenfile)
     fw = open(tokenfile, 'wb')
-    pickle.dump(token_response, fw)
+    pickle.dump(token_response.by_resource_server, fw)
     fw.close()
     os.chmod(tokenfile, 256)
     return
@@ -51,7 +49,8 @@ def read_token():
     if not os.path.isfile(tokenfile):
         request_token()
     fd = open(tokenfile, 'rb')
-    token_response = pickle.load(fd)
+    token_response = globus_sdk.auth.token_response
+    token_response.by_resource_server = pickle.load(fd)
     return token_response
 
 
